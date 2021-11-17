28cfcd7d4a274f787f42623fc221bcef0d92cbd9
Caíque Souto Lima <caiquesouto@hotmail.com>
2021-11-17 13:42:07 -0300

    Post Controller in progress

--- src/controller/PostController.ts
+++ src/controller/PostController.ts
@@ -1,2 +1,22 @@
 import { Request, Response } from "express"
 
+export class PostController {
+
+    public async getPosts(req: Request, res: Response): Promise<void> {
+
+        try {
+            
+            const tags = (req.query.tags as string).toLowerCase()
+            const sortBy = (req.query.sortBy as string).toLowerCase() || 'id'
+            const direction = (req.query.direction as string).toLowerCase() || 'asc'
+
+            console.log(`tags: ${tags}`)
+            console.log(`sortBy: ${sortBy}`)
+            console.log(`direction: ${direction}`)
+
+        } catch (error: any) {
+            res.status(error.statusCode).send({ error: error.message })
+        }
+
+    }
+}
--- src/routes/ApiRouter.ts
+++ src/routes/ApiRouter.ts
@@ -1,8 +1,11 @@
 import { Router } from "express"
 import { PingController } from "../controller/PingController"
+import { PostController } from "../controller/PostController"
 
 const pingController = new PingController()
+const postController = new PostController()
 
 export const apiRouter = Router()
 
 apiRouter.get('/ping', pingController.getPong)
+apiRouter.get('/post', postController.getPosts)
