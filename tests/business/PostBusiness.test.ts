bb2756c8af592405b1ab03fc755310546831529a
Caíque Souto Lima <caiquesouto@hotmail.com>
2021-11-18 12:31:41 -0300

    Unit tests for post business done

--- tests/business/PostBusiness.test.ts
+++ tests/business/PostBusiness.test.ts
@@ -1,3 +1,51 @@
 import { PostBusiness } from "../../src/business/PostBusiness"
-// import { PostDatabase } from "../../src/data/PostDatabase"
+import { PostDatabaseMock } from "../mocks/PostDatabaseMock"
+import { techPosts } from "../mocks/PostsMock"
 
+const postBusiness = new PostBusiness(new PostDatabaseMock())
+
+describe('Testing invalid query parameters', () => {
+
+    test('No tag is given', async () => {
+        expect.assertions(2)
+        try {
+            await postBusiness.getPostLogic(undefined, 'id', 'asc')
+        } catch (error: any) {
+            expect(error.message).toEqual('Tags parameter is required')
+            expect(error.statusCode).toBe(400)
+        }
+    })
+
+    test('Invalid sortBy parameter', async () => {
+        expect.assertions(2)
+        try {
+            await postBusiness.getPostLogic('test', 'hello', 'asc')
+        } catch (error: any) {
+            expect(error.message).toEqual('sortBy parameter is invalid')
+            expect(error.statusCode).toBe(400)
+        }
+    })
+
+    test('Invalid direction parameter', async () => {
+        expect.assertions(2)
+        try {
+            await postBusiness.getPostLogic('test', 'id', 'hello')
+        } catch (error: any) {
+            expect(error.message).toEqual('direction parameter is invalid')
+            expect(error.statusCode).toBe(400)
+        }
+    })
+})
+
+describe('Testing success case', () => {
+
+    test('All parameters are valid', async () => {
+        expect.assertions(1)
+        try {
+            const result = await postBusiness.getPostLogic('test', 'id', 'asc')
+            expect(result).toEqual(techPosts)
+        } catch (error) {
+            console.log(error)
+        }
+    })
+})
--- tests/mocks/PostDatabaseMock.ts
+++ tests/mocks/PostDatabaseMock.ts
@@ -1,48 +1,12 @@
-import axios from "axios"
-import { CustomError } from "../../src/error/CustomError"
 import { Post, PostData } from "../../src/model/Post"
+import { techPosts } from "./PostsMock"
 
-export class PostDatabase {
+export class PostDatabaseMock {
 
     public async getPostsFromSource(tags: string[], sortBy: string, direction: string): Promise<Post[]> {
 
-        try {
+        const mockPosts = techPosts.posts.map((post: PostData) => Post.toPostModel(post))
 
-            const urlArray: string[] = tags.map(tag => {
-                return `https://api.hatchways.io/assessment/blog/posts?tag=${tag}`
-            })
-
-            const allData = await Promise.all(urlArray.map(url => axios.get(url)))
-
-            const rawResult = allData?.map((item: any) => item.data.posts)
-
-            let allPosts: PostData[] = []
-
-            for (const postArray of rawResult) {
-                postArray.forEach((post: any) => {
-                    allPosts.push(post)
-                })
-            }
-
-            const uniquePosts = allPosts.filter((post, index, self) => {
-                return index === self.findIndex((p) => (p.id === post.id))
-            })
-
-            let sortedPosts: PostData[] = []
-            
-            if (direction === 'asc') {
-                sortedPosts = uniquePosts.sort((a: any, b: any) => (a[sortBy] > b[sortBy]) ? 1 : -1)
-            } else {
-                sortedPosts = uniquePosts.sort((a: any, b: any) => (a[sortBy] < b[sortBy]) ? 1 : -1)
-            }
-
-            const resultPosts = sortedPosts.map(post => Post.toPostModel(post))
-
-            return resultPosts
-
-        } catch (error) {
-            console.log(error)
-            throw new CustomError('Problem when getting posts from source API')
-        }
+        return mockPosts
     }
 }
